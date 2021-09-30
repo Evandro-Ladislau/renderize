8b3b96e89e79f763bd9c8880bc33051064916795
Evandro Ladislau <evandroladislau23@gmail.com>
2021-09-29 22:25:11 -0300

    DELETEI ARQUIVOS INUTILIZAVEIS , IMPLEMENTENTEI OS ARTIGOS NA PAGINA BLOG DE FORMA DINAMICA

--- app/sts/blog.php
+++ app/sts/blog.php
@@ -1,5 +1,5 @@
 <?php
-if(!isset($seguranca)){
+if (!isset($seguranca)) {
     exit;
 }
 include_once 'app/sts/header_blog.php'; // incui o arquivo header na pagina blog.
@@ -7,106 +7,98 @@ include_once 'app/sts/menu_blog.php'; // inclui o menu do blog
 require_once '../renderize/index.php';
 
 ?>
+
 <body>
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
-            <div class="container px-4 px-lg-5">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto py-4 py-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link px-lg-3 py-3 py-lg-4" href="<?php echo pg; ?>/index.php">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-lg-3 py-3 py-lg-4" href="#noticias">Notícias</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-lg-3 py-3 py-lg-4" href="#midia">Mídia</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <!-- Page Header-->
-        <header class="masthead" style="background-image: url('assets/img/home-bg.jpg')">
-            <div class="container position-relative px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-md-10 col-lg-8 col-xl-7">
-                        <div class="site-heading">
-                            <h1>Clean Blog</h1>
-                            <span class="subheading">A Blog Theme by Start Bootstrap</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
-        <!-- Main Content-->
-        <section>
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
         <div class="container px-4 px-lg-5">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto py-4 py-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link px-lg-3 py-3 py-lg-4" href="<?php echo pg; ?>/index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-lg-3 py-3 py-lg-4" href="#noticias">Notícias</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-lg-3 py-3 py-lg-4" href="#midia">Mídia</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Page Header-->
+    <header class="masthead" style="background-image: url('assets/img/blog/home-bg.jpg')">
+        <div class="container position-relative px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
-                    <!-- Post preview-->
-                    <div class="post-preview" id="noticias">
-                        <a href="post.html">
-                            <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
-                            <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on September 24, 2021
-                        </p>
+                    <div class="site-heading">
+                        <h1>Clean Blog</h1>
+                        <span class="subheading">A Blog Theme by Start Bootstrap</span>
                     </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html"><h2 class="post-title">I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.</h2></a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on September 18, 2021
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">Science has not yet mastered prophecy</h2>
-                            <h3 class="post-subtitle">We predict too much for the next year and yet far too little for the next ten.</h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on August 24, 2021
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">Failure is not an option</h2>
-                            <h3 class="post-subtitle">Many say exploration is part of our destiny, but it’s actually our duty to future generations.</h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on July 8, 2021
-                        </p>
-                    </div>
-                   
                 </div>
             </div>
         </div>
-        </section>
-        
-        <?php
-        include_once 'app/sts/footer_blog.php';
-        ?>   
+    </header>
+    <!-- Main Content-->
+    <section>
+        <div class="container px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5 justify-content-center">
+                <div class="col-md-10 col-lg-8 col-xl-7">
+                    <?php
+                    // ESSA QUERY BUSCA AS TALBELAS NO BANCO REFERENTE A TABELA STS_ARTIGOS
+                    $cmd = $pdo->query("SELECT id, titulo, descricao, slug, author, created FROM sts_artigos 
+                                    WHERE sts_situacoe_id=1
+                                    ORDER BY id DESC");
+                    $result_post = $cmd->fetchAll(PDO::FETCH_ASSOC);
 
-    </body>
+                    ?>
+                    <!-- Post preview-->
+                    <?php
+                    //PERCORRER A TABELA QUE VEIO EM UMA MATRIZ COMO RESULTADO E IMPRIMIR OS VALORES EM ORDEM DESC
+                    //NO LINK ELE MANDA PARA A PAGINA POST INFORMANDO O NOME DO SLUG NA URL DO SITE.
+                    if ($result_post) {
+                        for ($i = 0; $i < count($result_post); $i++) {
+
+                            foreach ($result_post[$i] as $k => $v) {
+                                if ($k == 'id') {
+
+                    ?>
+
+                                    <div class="post-preview" id="noticias">
+                                        <a href="<?php echo pg.'/post';?><?php echo "/".$result_post[$i]['slug'];?>">
+                                            <h2 class="post-title"><?php echo $result_post[$i]['titulo']?></h2>
+                                            <h3 class="post-subtitle"><?php echo $result_post[$i]['descricao']?></h3>
+                                        </a>
+                                        <p class="post-meta">
+                                            Posted by
+                                            <a href="#!"><?php echo $result_post[$i]['author']?></a>
+                                            <?php echo $result_post[$i]['created']?>
+                                        </p>
+                                    </div>
+                                    <!-- Divider-->
+                                    <hr class="my-4" />
+                    <?php
+
+
+                                }
+                            }
+                        }
+                    }
+
+
+                    ?>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php
+    include_once 'app/sts/footer_blog.php';
+    ?>
+
+</body>
